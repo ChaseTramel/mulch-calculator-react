0b74a03cce88e7efba693448908b37677a4da6da
K. Chase Tramel <chasetramel@hey.com>
2024-03-14 15:09:12 -0400

    Fixed accessibility issues with InputForm

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -7,29 +7,35 @@ function InputForm({ length, setLength, width, setWidth, depth, setDepth }) {
   
     return (
       <form className='formStyle' onSubmit={handleSubmit}>
-        <label>Length in Feet</label>
-        <br></br>
-        <input
+        <label htmlFor="lengthInput">Length in Feet
+          <input
+            id="lengthInput"
+            name="length"
             type="number"
             value={length}
             onChange={(e) => setLength(parseFloat(e.target.value))}
-        /> 
+          /> 
+        </label>
         <br></br>
-        <label>Width in Feet</label>
+        <label htmlFor="widthInput">Width in Feet
+          <input
+            id="widthInput"
+            name="width"
+            type="number"
+            value={width}
+            onChange={(e) => setWidth(parseFloat(e.target.value))}
+          />
+        </label>
         <br></br>
-        <input
-          type="number"
-          value={width}
-          onChange={(e) => setWidth(parseFloat(e.target.value))}
-        />
-        <br></br>
-        <label>Depth in Inches</label>
-        <br></br>
-        <input
-          type="number"
-          value={depth}
-          onChange={(e) => setDepth(parseFloat(e.target.value))}
-        />
+        <label htmlFor="depthInput">Depth in Inches
+          <input
+            id="depthInput"
+            name="depth"
+            type="number"
+            value={depth}
+            onChange={(e) => setDepth(parseFloat(e.target.value))}
+          />
+        </label>
         <br></br>
         <input type="button" value="Calculate!" />
       </form>
